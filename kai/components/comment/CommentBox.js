79be204dcd260a4e0f191fbb8d75d29eafc198bc
Tiffany J <tiffapedia@gmail.com>
2017-06-13 20:36:48 -0700

    chore: Add necessary libraries

---
+++ kai/components/comment/CommentBox.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Comment from "comment";
+
+class CommentBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    _getComments() {
+        const commentList = [
+            {id: 1, author: "Morgan McCircuit", message: "Great picture!"},
+            {id: 2, author: "Bending Bender", message: "Excellent stuff"}
+        ];
+        return commentList.map( (comment) => {
+            return (
+                <Comment author={comment.author} message={comment.message} key={comment.id}/>
+            );
+         });
+    }
+
+    render() {
+        const comments = this._getComments();
+        return(
+            <div className="comment-box">
+                <h3>Comments</h3>
+                <h4 className="comment-count">{comments.length} comments</h4>
+                <div className="comemnt-list">
+                    {comments}
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default CommentBox;
